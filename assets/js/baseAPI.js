10daeade220dbcf0fa89c1db987b436a63f02998
grj <3303272844@qq.com>
2022-06-11 09:49:09 +0800

    完成主页功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,7 +2,25 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter((options) => {
-    console.log(options);
+  console.log(options);
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   options.url = "http://www.liulongbin.top:3007" + options.url;
+  //includes()方法判断一个字符串是否存在于另一个字符串里，有就返回true，否则false
+  // indexOf()方法判断一个字符串是否存在于另一个字符串里，有就返回下标，否则返回-1e
+  if (options.url.includes("/my/")) {
+    options.headers = {
+      Authorization: localStorage.getItem("token"),
+    };
+  }
+  //权限校验
+  options.complete = (res) => {
+    console.log(res);
+    if (
+      res.responseJSON.status === 1 &&
+      res.responseJSON.message === "身份认证失败！"
+    ) {
+      localStorage.removeItem("token");
+      location.href = "/login.html";
+    }
+  };
 });
---
+++ assets/js/index.js
@@ -0,0 +1,59 @@
+//获取用户基本信息
+function getUserInfo() {
+  $.ajax({
+    method: "GET",
+    url: "/my/userinfo",
+    // headers: {
+    //   Authorization: localStorage.getItem("token"),
+    // },
+    success: (res) => {
+      if (res.status !== 0) return layer.msg(res.message);
+      layer.msg("获取用户信息成功");
+      // console.log(res);
+      readerAvatar(res.data);
+    },
+    // complete: (res) => {
+    //   console.log(res);
+    //   if (
+    //     res.responseJSON.status === 1 &&
+    //     res.responseJSON.message === "身份认证失败！"
+    //   ) {
+    //     localStorage.removeItem("token");
+    //     location.href = "/login.html";
+    //   }
+    // },
+  });
+}
+
+//退出登录
+$("#btnLogout").click(function () {
+  layui.layer.confirm(
+    "确定退出登录？",
+    { icon: 3, title: "" },
+    function (index) {
+      // 清空本地存储里面的 token
+      localStorage.removeItem("token");
+      // 重新跳转到登录页面
+      location.href = "/login.html";
+    }
+  );
+});
+
+//获取用户列表
+getUserInfo();
+
+const readerAvatar = (user) => {
+  //渲染欢迎语
+  const name = user.nickname || user.username;
+  $("#welcome").html(`欢迎 ${name}`);
+  //按需渲染头像
+  if (user.user_pic !== null) {
+    $(".layui-nav-img").attr("src", user.user_pic).show();
+    $(".text-avatar").hide();
+  } else {
+    $(".layui-nav-img").hide();
+    //用户名的首字母设置为大写渲染到文字框里
+    let first = name[0].toUpperCase();
+    $(".text-avatar").html(first).show();
+  }
+};
